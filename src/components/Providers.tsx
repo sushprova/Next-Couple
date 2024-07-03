f8b588f7bbfa1d2f0b518ba8cc2c24acfa269a8d
sushprova <sushthepro@gmail.com>
2024-07-03 00:26:12 -0500

    Setting up

---
+++ src/components/Providers.tsx
@@ -0,0 +1,6 @@
+import { NextUIProvider } from "@nextui-org/react";
+import React, { ReactNode } from "react";
+
+export default function Providers({ children }: { children: ReactNode }) {
+  return <NextUIProvider>{children}</NextUIProvider>;
+}
